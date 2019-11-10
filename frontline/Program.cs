ceb86456b7bdde20f64ccdb3f50e3dbddf3ceb6b
Claytorpedo <claytorpedo@gmail.com>
2019-11-09 21:20:43 -0800

    Added initial functionality.

--- frontline/Program.cs
+++ frontline/Program.cs
@@ -1,12 +1,131 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+// TODO: Launch the first new image for each thing updated?
 
 namespace frontline
 {
     class Program
     {
+        public static List<string> helpStrings = new List<string>{ "help", "?" };
+        static readonly HttpClient httpClient = new HttpClient();
+        static readonly WebClient webClient = new WebClient();
+        static Subscriptions subscriptions = null;
+        static int updatedSubscriptions = 0;
+        static int updatedFiles = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0 || helpStrings.Contains(args[0]))
+            {
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("frontline");
+                Console.WriteLine("\nPlease supply the subscriptions file.\nUsage: frontline <path/to/my/subscriptions.xml>");
+                Console.WriteLine("----------------------------------------------------");
+                return;
+            }
+            string xmlFilePath = args[0];
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine("Error: Subscription file \"{0}\" not found.", xmlFilePath);
+                return;
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(xmlFilePath))
+                    subscriptions = (Subscriptions)new XmlSerializer(typeof(Subscriptions)).Deserialize(reader);
+
+                foreach (var sub in subscriptions.Infos)
+                {
+                    Console.WriteLine("Updating subscription \"{0}\" (which is type \"{1}\")", sub.GetName(), nameof(sub));
+                    RunSubscription(sub);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (subscriptions != null && updatedFiles > 0)
+            {
+                string tempFile = xmlFilePath + ".tmp";
+                using (StreamWriter writer = new StreamWriter(tempFile))
+                    new XmlSerializer(typeof(Subscriptions)).Serialize(writer, subscriptions);
+                File.Delete(xmlFilePath);
+                File.Move(tempFile, xmlFilePath);
+            }
+
+            Console.WriteLine("Updated {0} subscriptions with {1} files.", updatedSubscriptions, updatedFiles);
+        }
+
+        static bool RunSubscription(SubscriptionInfo sub)
+        {
+            UpdateResult result;
+            bool updated = false;
+            while (true)
+            {
+                result = GetUpdate(sub).GetAwaiter().GetResult();
+                if (result != UpdateResult.ContentFound)
+                    break;
+                sub.Increment();
+                ++updatedFiles;
+                updated = true;
+            }
+
+            if (updated)
+                ++updatedSubscriptions;
+
+            if (result == UpdateResult.Error)
+                Console.WriteLine("Encountered an error while updating feed \"{0}\".", sub.GetName());
+
+            return result == UpdateResult.UpToDate;
+        }
+        enum UpdateResult
+        {
+            UpToDate,
+            ContentFound,
+            Error
+        }
+        static async Task<UpdateResult> GetUpdate(SubscriptionInfo sub)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync(sub.GetNextPageUrl());
+                if (!response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine("No page yet for {0}. Stopping...", sub.GetNextPageUrl());
+                        return UpdateResult.UpToDate;
+                    }
+
+                    Console.WriteLine("Failed to get content at url {0}. With status code {1}, reason \"{2}\" ", sub.GetNextPageUrl(), response.StatusCode, response.ReasonPhrase);
+                    return UpdateResult.Error;
+                }
+
+                string body = await response.Content.ReadAsStringAsync();
+                SubscriptionInfo.ContentInfo imageInfo = sub.FindImage(body);
+
+                string path = Path.Combine(subscriptions.SaveDir, imageInfo.localFilePath);
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                Console.WriteLine("Downloading image from {0} and saving to \"{1}\".", imageInfo.imageURL, path);
+
+                webClient.DownloadFile(imageInfo.imageURL, path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return UpdateResult.Error;
+            }
+            return UpdateResult.ContentFound;
         }
     }
 }
---
+++ frontline/SubscriptionInfo.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+
+namespace frontline
+{
+    public abstract class SubscriptionInfo
+    {
+        public const string ImageExtensions = "png|jpg|jpeg";
+        public abstract string GetNextPageUrl();
+        public struct ContentInfo
+        {
+            public string imageURL;
+            public string localFilePath;
+        }
+        public abstract ContentInfo FindImage(string pageBody);
+        public abstract string GetName();
+        public abstract void Increment();
+    }
+    public class SaizensenInfo : SubscriptionInfo
+    {
+        public override string GetNextPageUrl()
+        {
+            return baseLink + GetPageIndex() + ".html";
+        }
+        public override ContentInfo FindImage(string pageBody)
+        {
+            var info = new ContentInfo();
+            // Link format:           <baselink><folders and comic name><page#>.<an id of some kind>.<img ext -- always png?>
+            var expr = string.Format("{0}[a-z0-9-\\/\\.]+?{1}\\.[a-z0-9-\\/\\.]+?\\.{2}", baseLink, GetPageIndex(), ImageExtensions);
+            var match = Regex.Match(pageBody, expr, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                throw new InvalidDataException(string.Format("Failed to find regex match for page {0} of {1}. Has the format changed?", pageIndex, name));
+            info.imageURL = match.Value;
+            var ext = info.imageURL.Substring(info.imageURL.LastIndexOf('.'));
+            info.localFilePath = Path.Combine(name, GetPageIndex() + ext);
+            return info;
+        }
+        public override string GetName()
+        {
+            return name;
+        }
+        public override void Increment()
+        {
+            ++pageIndex;
+        }
+
+        public string GetPageIndex()
+        {
+            return pageIndex.ToString("D" + pageNumPadding);
+        }
+
+        public string baseLink;
+        public string name;
+        public int pageIndex;
+        public int pageNumPadding;
+    }
+    public class Subscriptions
+    {
+        public string SaveDir;
+        [XmlArrayItem(typeof(SaizensenInfo))]
+        public SubscriptionInfo[] Infos;
+    }
+}
